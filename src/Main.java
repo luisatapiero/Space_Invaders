8c10307882e02a5431dd1f01a1e99fbc1bbd24ee
Luisa Tapiero <luisatapiero45@gmail.com>
2021-03-08 23:42:24 -0500

    movimiento nave

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,65 @@
 
 import processing.core.PApplet;
 import processing.core.PImage;
-public class Main extends PApplet{
-	
+
+public class Main extends PApplet {
+
 	private Nave nave;
 	PImage imgInicio;
 	PImage imgInstrucciones;
 	PImage imgResumen;
 	PImage imgJuego;
 	int tiempo = 0;
-	
-	
-	int pantalla = 1;
-	
-	public Main() {	
 
-	}
+
+	int pantalla = 1;
 
 	public static void main(String[] args) {
 		PApplet.main(Main.class.getName());
-		
+
 	}
-	
+
 	public void settings() {
-		size (600, 900);
+		size(600, 900);
 	}
-	
-	public void setup() {
-		nave = new Nave(this);
+
+	public void setup() {	
 		cargarFondos();
-		
-		
+		nave = new Nave(this);
 	}
-	
+
 	public void draw() {
+		
 		imageMode(CENTER);
-		//System.out.println(mouseX);
+		// System.out.println(mouseX);
 		cambioPantallas();
 		
 
-		
 	}
-	
+
 	public void mousePressed() {
-		clickBoton();	
-		
+		clickBoton();
+
 	}
 	
+	public void keyPressed() {
+		if (nave.getPosX()<600) {
+		if (keyCode == LEFT) {
+			nave.setPosX (nave.getPosX() - 10);
+			
+		} 
+		if (nave.getPosX()>0) {
+			if (keyCode == RIGHT) {
+				nave.setPosX (nave.getPosX() + 10);
+				
+			}
+		}
+		
+		}
+		
+	}
+
+
 	public void cargarFondos() {
 		imgInicio = loadImage("lib/inicio.jpg");
 		imgInicio.resize(600, 900);
@@ -57,74 +70,74 @@ public class Main extends PApplet{
 		imgJuego = loadImage("lib/juego.jpg");
 		imgJuego.resize(600, 900);
 	}
-	
+
 	public void cambioPantallas() {
 		switch (pantalla) {
 		case 1:
-			background (imgInicio);
+			background(imgInicio);
 			break;
-			
+
 		case 2:
-			background (imgJuego);
+			background(imgJuego);
 			nave.draw();
 			tiempo();
 			break;
-			
+
 		case 3:
-			background (imgInstrucciones);
+			background(imgInstrucciones);
 			break;
-			
+
 		case 4:
 			exit();
 			break;
-			
+
 		case 5:
-			background (imgResumen);
+			background(imgResumen);
 			break;
 
 		}
-		
+
 	}
-	
+
 	public void clickBoton() {
 		switch (pantalla) {
 		case 1:
-			if (mouseX > 188 && mouseX < 422 && mouseY > 439 && mouseY < 506){
+			if (mouseX > 188 && mouseX < 422 && mouseY > 439 && mouseY < 506) {
 				pantalla = 2;
-			} else if (mouseX > 188 && mouseX < 422 && mouseY > 555 && mouseY < 621){
+			} else if (mouseX > 188 && mouseX < 422 && mouseY > 555 && mouseY < 621) {
 				pantalla = 3;
-			}else if (mouseX > 188 && mouseX < 422 && mouseY > 670 && mouseY < 736){
-					pantalla = 4;
+			} else if (mouseX > 188 && mouseX < 422 && mouseY > 670 && mouseY < 736) {
+				pantalla = 4;
 			}
 			break;
-			
+
 		case 2:
-			background (imgJuego);
+			background(imgJuego);
 			break;
-			
+
 		case 3:
-			if (mouseX > 46 && mouseX < 217 && mouseY > 108 && mouseY < 174){
+			if (mouseX > 46 && mouseX < 217 && mouseY > 108 && mouseY < 174) {
 				pantalla = 1;
 			}
 			break;
-			
+
 		case 4:
-			background (imgResumen);
-			if (mouseX > 170 && mouseX < 418 && mouseY > 522 && mouseY < 588){
+			background(imgResumen);
+			if (mouseX > 170 && mouseX < 418 && mouseY > 522 && mouseY < 588) {
 				pantalla = 2;
 			}
 			break;
-		
-			}
+
+		}
 	}
-	
+
 	public void tiempo() {
-		int tiempo = millis()/1000;
-		textSize(25); 
+		int tiempo = millis() / 1000;
+		textSize(25);
 		fill(245, 245, 245);
-		text("Tiempo: "+tiempo,410,60);
+		text("Tiempo: " + tiempo, 410, 60);
 	}
+
 	
-
-
+	
 }
--- src/Nave.java
+++ src/Nave.java
@@ -2,27 +2,46 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Nave {
-	
-	private int dir, posX;
+
+	private int posX = 300;
 	PImage nave;
 	private PApplet app;
-	
-	public Nave(PApplet app) {
+	public int dir = 0;
 
+	public Nave( PApplet app) {
+	
 		this.app = app;
-		
+
 	}
+
 	
-	
-	private void cargarNave(){
+
+	private void cargarNave() {
 		nave = app.loadImage("lib/nave.png");
 		nave.resize(158, 150);
+		app.image(nave, posX, 783);
 	}
-	
+
 	public void draw() {
+		
 		cargarNave();
-		app.image(nave, 300, 783);
-	
-	
+
+
+		
+
 	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	
+	
+	
+		 
+	
 }
